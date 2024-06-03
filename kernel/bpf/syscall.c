ae45ae71701a49e7805d931d333154aaccaed690
Alfred Neumayer <dev.beidl@gmail.com>
2024-06-03 17:51:21 +0200

    kernel: bpf: Allow attaching programs with CAP_SYS_ADMIN
    snap-confine needs privileges to setup it's device cgroup checks.
    For that, allow CAP_SYS_ADMIN executables do it's job like with
    an upstream or Ubuntu-flavoured kernel.

--- kernel/bpf/syscall.c
+++ kernel/bpf/syscall.c
@@ -2996,6 +2996,9 @@ static int bpf_prog_attach(const union bpf_attr *attr)
 	struct bpf_prog *prog;
 	int ret;
 
+	if (!capable(CAP_NET_ADMIN) && !capable(CAP_SYS_ADMIN))
+		return -EPERM;
+
 	if (CHECK_ATTR(BPF_PROG_ATTACH))
 		return -EINVAL;
 
@@ -3050,6 +3053,9 @@ static int bpf_prog_detach(const union bpf_attr *attr)
 {
 	enum bpf_prog_type ptype;
 
+	if (!capable(CAP_NET_ADMIN) && !capable(CAP_SYS_ADMIN))
+		return -EPERM;
+
 	if (CHECK_ATTR(BPF_PROG_DETACH))
 		return -EINVAL;
 
@@ -3081,8 +3087,9 @@ static int bpf_prog_detach(const union bpf_attr *attr)
 static int bpf_prog_query(const union bpf_attr *attr,
 			  union bpf_attr __user *uattr)
 {
-	if (!capable(CAP_NET_ADMIN))
+	if (!capable(CAP_NET_ADMIN) && !capable(CAP_SYS_ADMIN))
 		return -EPERM;
+
 	if (CHECK_ATTR(BPF_PROG_QUERY))
 		return -EINVAL;
 	if (attr->query.query_flags & ~BPF_F_QUERY_EFFECTIVE)
